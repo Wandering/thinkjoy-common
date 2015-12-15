9dd7ffa45401b4969dde967e295f19c8589db055
惠林 <huilin@imzhiliao.com>
2015-12-15 20:56:30 +0800

    no message

--- thinkjoy-domain-common/src/main/java/cn/thinkjoy/common/domain/BaseDomain.java
+++ thinkjoy-domain-common/src/main/java/cn/thinkjoy/common/domain/BaseDomain.java
@@ -20,7 +20,7 @@ public class BaseDomain<T> implements Serializable {
     private T              id;
 
     public T getId() {
-        if( id instanceof BigInteger || id instanceof Integer)
+        if( id instanceof BigInteger)
             return (T)Long.valueOf(String.valueOf(this.id));
         else
             return this.id;
