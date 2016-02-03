e6ffd039bcd895795ce16ab9df5af2da33afd95f
xule <xule@thinkjoy.cn>
2016-02-03 09:29:43 +0800

    facade层的PersistenceProvider使用接口注入服务，以支持注解式事务

--- thinkjoy-biz-common/src/main/java/cn/thinkjoy/common/service/impl/AbstractPersistenceProvider.java
+++ thinkjoy-biz-common/src/main/java/cn/thinkjoy/common/service/impl/AbstractPersistenceProvider.java
@@ -3,6 +3,7 @@ package cn.thinkjoy.common.service.impl;
 import cn.thinkjoy.common.dao.IBaseDAO;
 import cn.thinkjoy.common.domain.BaseDomain;
 import cn.thinkjoy.common.mybatis.core.mybatis.criteria.Criteria;
+import cn.thinkjoy.common.service.IBaseService;
 import cn.thinkjoy.common.service.IPersistenceProvider;
 import cn.thinkjoy.common.utils.SqlOrderEnum;
 
@@ -17,24 +18,9 @@ import java.util.Map;
  * @author qyang
  * @since v0.0.1
  */
-public class AbstractPersistenceProvider implements IPersistenceProvider {
+public abstract class AbstractPersistenceProvider implements IPersistenceProvider {
 
-    @Override
-    public int insertMap(Map entityMap) {
-        //other biz logic process
-
-
-
-        return 0;
-    }
-
-    @Override
-    public int updateMap(Map entityMap) {
-        //other biz logic process
-
-
-        return 0;
-    }
+    public abstract IBaseService getService();
 
     @Override
     public IBaseDAO getDao() {
@@ -42,193 +28,202 @@ public class AbstractPersistenceProvider implements IPersistenceProvider {
     }
 
     @Override
+    public int insertMap(Map entityMap) {
+        return getService().insertMap(entityMap);
+    }
+
+    @Override
+    public int updateMap(Map entityMap) {
+        return getService().updateMap(entityMap);
+    }
+
+    @Override
     public int add(BaseDomain entity) {
-        return 0;
+        return getService().add(entity);
     }
 
     @Override
     public int edit(BaseDomain entity) {
-        return 0;
+        return getService().edit(entity);
     }
 
     @Override
     public int delete(Object id) {
-        return 0;
+        return getService().delete(id);
     }
 
     @Override
     public BaseDomain view(Object id) {
-        return null;
+        return getService().view(id);
     }
 
     @Override
     public int insert(BaseDomain entity) {
-        return 0;
+        return getService().insert(entity);
     }
 
     @Override
     public int update(BaseDomain entity) {
-        return 0;
+        return getService().update(entity);
     }
 
     @Override
     public int updateNull(BaseDomain entity) {
-        return 0;
+        return getService().updateNull(entity);
     }
 
     @Override
     public int deleteById(Object id) {
-        return 0;
+        return getService().deleteById(id);
     }
 
     @Override
     public int deleteByIds(List list) {
-        return 0;
+        return getService().deleteByIds(list);
     }
 
     @Override
     public int deleteByProperty(String property, Object value) {
-        return 0;
+        return getService().deleteByProperty(property, value);
     }
 
     @Override
     public BaseDomain fetch(Object id) {
-        return null;
+        return getService().fetch(id);
     }
 
     @Override
     public BaseDomain findOne(String property, Object value) {
-        return null;
+        return getService().findOne(property, value);
     }
 
     @Override
     public List findList(String property, Object value) {
-        return null;
+        return getService().findList(property, value);
     }
 
     @Override
     public List findList(String property, Object value, String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().findList(property, value, orderBy, sqlOrderEnum);
     }
 
     @Override
     public List findAll() {
-        return null;
+        return getService().findAll();
     }
 
     @Override
     public List findAll(String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().findAll(orderBy, sqlOrderEnum);
     }
 
     @Override
     public Object selectMaxId() {
-        return null;
+        return getService().selectMaxId();
     }
 
     @Override
     public void updateOrSave(BaseDomain baseDomain, Object id) {
-
+        getService().updateOrSave(baseDomain, id);
     }
 
     @Override
     public BaseDomain selectOne(String mapperId, Object obj) {
-        return null;
+        return getService().selectOne(mapperId, obj);
     }
 
     @Override
     public List selectList(String mapperId, Object obj) {
-        return null;
+        return getService().selectList(mapperId, obj);
     }
 
     @Override
     public List findByCriteria(Criteria criteria) {
-        return null;
+        return getService().findByCriteria(criteria);
     }
 
     @Override
     public BaseDomain findOneByCriteria(Criteria criteria) {
-        return null;
+        return getService().findOneByCriteria(criteria);
     }
 
     @Override
     public int updateByCondition(Map updateMap, Map conditionMap) {
-
-        return 0;
+        return getService().updateByCondition(updateMap, conditionMap);
     }
 
     @Override
     public int count(Map condition) {
-        return 0;
+        return getService().count(condition);
     }
 
     @Override
     public BaseDomain queryOne(Map condition, String orderBy, SqlOrderEnum sqlOrderEnum, Map selector) {
-        return null;
+        return getService().queryOne(condition, orderBy, sqlOrderEnum, selector);
     }
 
     @Override
     public BaseDomain queryOne(Map condition, String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().queryOne(condition, orderBy, sqlOrderEnum);
     }
 
     @Override
     public BaseDomain queryOne(Map condition) {
-        return null;
+        return getService().queryOne(condition);
     }
 
     @Override
     public List queryList(Map condition, String orderBy, String sortBy, Map selector) {
-        return null;
+        return getService().queryList(condition, orderBy, sortBy, selector);
     }
 
     @Override
     public List queryList(Map condition, String orderBy, String sortBy) {
-        return null;
+        return getService().queryList(condition, orderBy, sortBy);
     }
 
     @Override
     public List like(Map condition, String orderBy, SqlOrderEnum sqlOrderEnum, Map selector) {
-        return null;
+        return getService().like(condition, orderBy, sqlOrderEnum, selector);
     }
 
     @Override
     public List like(Map condition, String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().like(condition, orderBy, sqlOrderEnum);
     }
 
     @Override
     public List like(Map condition) {
-        return null;
+        return getService().like(condition);
     }
 
     @Override
     public List queryPage(Map condition, int offset, int rows, String orderBy, SqlOrderEnum sqlOrderEnum, Map selectorpage) {
-        return null;
+        return getService().queryPage(condition, offset, rows, orderBy, sqlOrderEnum, selectorpage);
     }
 
     @Override
     public List queryPage(Map condition, int offset, int rows, String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().queryPage(condition, offset, rows, orderBy, sqlOrderEnum);
     }
 
     @Override
     public List queryPage(Map condition, int offset, int rows) {
-        return null;
+        return getService().queryPage(condition, offset, rows);
     }
 
     @Override
     public int deleteByCondition(Map condition) {
-        return 0;
+        return getService().deleteByCondition(condition);
     }
 
     @Override
     public List listByPage(Map condition, int offset, int rows, String orderBy, SqlOrderEnum sqlOrderEnum) {
-        return null;
+        return getService().listByPage(condition, offset, rows, orderBy, sqlOrderEnum);
     }
 
     @Override
     public List listByPage(Map condition, int offset, int rows) {
-        return null;
+        return getService().listByPage(condition, offset, rows);
     }
 }
